38b871448beda4654f64a8fceab4556f203cd29a
Reche Soares <rechesoares13@gmail.com>
2025-02-10 16:43:14 +0530

    auth routes init

--- apps/api/src/routes/v1/auth/index.ts
+++ apps/api/src/routes/v1/auth/index.ts
@@ -1,6 +1,21 @@
 import { Router } from "express";
+import { loginRouter } from "./loginRouter";
+import { logoutRouter } from "./logoutRouter";
+import { signupRouter } from "./signupRouter";
+import { googleRouter } from "./googleRouter";
+
 const router: Router = Router();
 
-router.use("/");
+// LOGIN
+router.get("/login", loginRouter);
+
+// SIGNUP
+router.get("/signup", signupRouter);
+
+// GOOGLE LOGIN
+router.get("/oauth2/google", googleRouter);
+
+// LOGOUT+
+router.get("/logout", logoutRouter);
 
 export default router;
