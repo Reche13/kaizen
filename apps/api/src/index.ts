37e53fcf271a0fda0abebd45d3d852087c8de716
Reche Soares <rechesoares13@gmail.com>
2025-02-10 16:39:03 +0530

    auth router initialized

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,13 +2,15 @@ import { config } from "dotenv";
 config();
 import express, { NextFunction, Request, Response } from "express";
 
+import v1Router from "./routes/v1";
+
 const app = express();
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.send("<h1>API is running</h1>");
 });
 
+app.use("/api/v1", v1Router);
+
 const PORT = process.env.PORT ?? 8000;
-app.listen(PORT, () => {
-  console.log("Server listening on port", PORT);
-});
+app.listen(PORT, () => console.log(`api running on port ${PORT}...`));
